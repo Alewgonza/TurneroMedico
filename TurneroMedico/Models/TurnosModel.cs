cf4b45e6ea14834b6646101716b29c72a0eab64c
alewgonza <alewgonza@gmail.com>
2024-11-06 22:00:00 -0300

    agregadas validaciones iniciales de TurnosModel

--- TurneroMedico/Models/TurnosModel.cs
+++ TurneroMedico/Models/TurnosModel.cs
@@ -9,18 +9,42 @@ namespace TurneroMedico.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Por favor complete su nombre y apellido"), MaxLength(60)]
+        [Display(Name = "Nombre y Apellido")]
         public string NombreApellido { get; set; }
-        public string Dni { get; set; }
+
+        [Required(ErrorMessage = "Por favor complete su DNI")]
+        [Display(Name = "DNI:")]
+        [MaxLength(8), MinLength(8)]
+        public int Dni { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Por favor ingrese su email")]
+        [Display(Name = "EMAIL:")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email inválido.")]
         public string Email { get; set; }
 
-        public string Telefono { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Por favor ingrese su nro de teléfono")]
+        [Display(Name = "Número de telefono")]
+        [MaxLength(15), MinLength(10)]
+        public int Telefono { get; set; }
 
-        [Display(Name = "Fecha del turno")]
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "Por favor seleccione un turno")]
+        [Display(Name = "Fecha del turno:")]
         public DateTime FechaTurno { get; set; }
+
         [EnumDataType(typeof(Especialidades))]
+        [Required(ErrorMessage = "Por favor seleccione una especialidad")]
+        [Display(Name = "Especialidad:")]
         public Especialidades EspecialidadElegida { get; set; }
 
         [EnumDataType(typeof(Doctores))]
+        [Required(ErrorMessage = "Por favor seleccione un doctor")]
+        [Display(Name = "Doctor:")]
         public Doctores DoctorElegido { get; set; }
     }
 }
